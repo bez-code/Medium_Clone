a8691ee4adc9874d93fa8c5bbc0658850fd800b9
bez-code <BEHZAD.P.ashrafi@gmail.com>
2024-11-16 20:55:49 +0300

    "Added register component with form validation, routing, and styling"

--- Medium_clone/src/app/app-routing.module.ts
+++ Medium_clone/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './auth/component/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Medium_clone/src/app/auth/component/register/register.component.ts
+++ Medium_clone/src/app/auth/component/register/register.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+ 
+
+  constructor(private fb: FormBuilder) { }
+
+  signUpForm = this.fb.nonNullable.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      console.log('Form Submitted', this.signUpForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }
